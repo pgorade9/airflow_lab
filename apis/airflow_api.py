749e96257f5846db85725b455831998e5c8b636a
pgorade <pgorade@slb.com>
2025-04-06 14:23:20 +0530

    code refactor

--- apis/airflow_api.py
+++ apis/airflow_api.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Literal
 
 from fastapi import APIRouter, Query
 
@@ -9,10 +10,14 @@ from service.airflow_service import (get_dag_status_from_id, async_trigger_dag,
 airflow_router = APIRouter(prefix="/airflow2/api/v1",
                            tags=["Airflow APIs (environment dependant)"], )
 
+env_list = [key for key in keyvault.keys() if
+            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None]
+
+data_partition_list = list({keyvault[key].get("data_partition_id") for key in keyvault.keys() if
+                            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None})
 
 @airflow_router.get("/dags/run_status")
-def get_dag_run_status(env: str = Query(None, description="Environment",
-                                        enum=keyvault["envs-ltops"] + keyvault["envs"]),
+def get_dag_run_status(env: Literal[*env_list] = Query(...),
                        dag: str = Query(None, description="DAG Name",
                                         enum=keyvault["dags-ltops"]),
                        run_id: str = Query("990de971-4d15-40eb-b635-d599ccdc169a", description="Run Id")):
@@ -20,8 +25,7 @@ def get_dag_run_status(env: str = Query(None, description="Environment",
 
 
 @airflow_router.get("/dags/query")
-def get_dag_run_status(env: str = Query(None, description="Environment",
-                                        enum=keyvault["envs-ltops"] + keyvault["envs"]),
+def get_dag_run_status(env: Literal[*env_list] = Query(...),
                        dag: str = Query(None, description="DAG Name",
                                         enum=keyvault["dags-ltops"]),
                        date_string: str = Query("2025-03-11T11:13:28.992416+00:00"),
@@ -30,8 +34,7 @@ def get_dag_run_status(env: str = Query(None, description="Environment",
 
 
 @airflow_router.get("/dags/logs")
-def get_dag_logs(env: str = Query("evd-ltops", description="Environment",
-                                  enum=keyvault["envs-ltops"] + keyvault["envs"]),
+def get_dag_logs(env: Literal[*env_list] = Query(...),
                  dag: str = Query("shapefile_ingestor_wf_status_gsm", description="DAG Name",
                                   enum=keyvault["dags-ltops"]),
                  run_id: str = Query("dc7399e8-c995-4cd1-b9f3-b6ada8836d54", description="Run Id"),
@@ -40,8 +43,7 @@ def get_dag_logs(env: str = Query("evd-ltops", description="Environment",
 
 
 @airflow_router.post("/dags/runs")
-def send_batch_jobs_to_airflow(env: str = Query(None, description="Environment",
-                                                enum=keyvault["envs-ltops"]),
+def send_batch_jobs_to_airflow(env: Literal[*env_list] = Query(...),
                                dag: str = Query(None, description="DAG Name",
                                                 enum=keyvault["dags-ltops"]),
                                batch_size: int = Query(1, description="Batch Size"),
--- apis/service_bus_api.py
+++ apis/service_bus_api.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import Literal
 
 from azure.servicebus.management import SubscriptionRuntimeProperties
 from fastapi import Query
@@ -12,11 +13,15 @@ from configuration import keyvault
 fc_router = routing.APIRouter(prefix="/service_bus",
                               tags=["Service Bus Messaging APIs (data-partition-dependant)"], )
 
+env_list = [key for key in keyvault.keys() if
+            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None]
+
+data_partition_list = list({keyvault[key].get("data_partition_id") for key in keyvault.keys() if
+                            isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None})
 
 @fc_router.get("/topic_info")
-def get_topic_info(env: str = Query(None, description="Environment",
-                                    enum=keyvault["envs-ltops"]),
-                   data_partition_id: str = "admedev01-dp3"):
+def get_topic_info(env: Literal[*env_list] = Query(...),
+                   data_partition_id: Literal[*data_partition_list] = Query(...)):
     subscription_info = asyncio.run(get_service_bus_topic_info(env, data_partition_id))
     if isinstance(subscription_info, SubscriptionRuntimeProperties):
         return {
@@ -30,9 +35,8 @@ def get_topic_info(env: str = Query(None, description="Environment",
 
 
 @fc_router.post("/send_bulk_messages")
-def send_bulk_messages(env: str = Query(None, description="Environment",
-                                        enum=keyvault["envs-ltops"]),
-                       data_partition_id: str = "admedev01-dp3",
+def send_bulk_messages(env: Literal[*env_list] = Query(...),
+                       data_partition_id: Literal[*data_partition_list] = Query(...),
                        dag: str = Query(None, description="DAG Name",
                                         enum=keyvault["dags-ltops"]),
                        batch_size: int = Query(1, description="Batch Size"),
@@ -41,21 +45,18 @@ def send_bulk_messages(env: str = Query(None, description="Environment",
 
 
 @fc_router.get("/complete_active_messages")
-def complete_active_messages(env: str = Query(None, description="Environment",
-                                              enum=keyvault["envs-ltops"]),
-                             data_partition_id: str = "admedev01-dp3"):
+def complete_active_messages(env: Literal[*env_list] = Query(...),
+                             data_partition_id: Literal[*data_partition_list] = Query(...)):
     return asyncio.run(receive_and_complete_active_messages(env, data_partition_id))
 
 
 @fc_router.get("/delete_dead_letter_queue_messages")
-def delete_dead_letter_queue_messages(env: str = Query(None, description="Environment",
-                                                       enum=keyvault["envs-ltops"]),
-                                      data_partition_id: str = "admedev01-dp3"):
+def delete_dead_letter_queue_messages(env: Literal[*env_list] = Query(...),
+                                      data_partition_id: Literal[*data_partition_list] = Query(...)):
     return asyncio.run(receive_and_delete_dead_letter_queue_messages(env, data_partition_id))
 
 
 @fc_router.get("/delete_active_messages")
-def delete_active_messages(env: str = Query(None, description="Environment",
-                                            enum=keyvault["envs-ltops"]),
-                           data_partition_id: str = "admedev01-dp3"):
+def delete_active_messages(env: Literal[*env_list] = Query(...),
+                           data_partition_id: Literal[*data_partition_list] = Query(...)):
     return asyncio.run(receive_and_delete_active_messages(env, data_partition_id))
