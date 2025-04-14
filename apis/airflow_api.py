909aa4a06c0cf9a9e75cea3ef64d8900d8db7a96
pgorade <pgorade@slb.com>
2025-04-14 18:27:03 +0530

    code refactor

--- apis/airflow_api.py
+++ apis/airflow_api.py
@@ -16,19 +16,18 @@ env_list = [key for key in keyvault.keys() if
 data_partition_list = list({keyvault[key].get("data_partition_id") for key in keyvault.keys() if
                             isinstance(keyvault[key], dict) and keyvault[key].get("data_partition_id") is not None})
 
+dag_list = keyvault['dags-ltops'] + keyvault['dags-seismic-ltops']
 
 @airflow_router.get("/dags/run_status")
 def get_dag_run_status(env: Literal[*env_list] = Query(...),
-                       dag: str = Query(None, description="DAG Name",
-                                        enum=keyvault["dags-ltops"]),
+                       dag: Literal[*dag_list] = Query(...),
                        run_id: str = Query("990de971-4d15-40eb-b635-d599ccdc169a", description="Run Id")):
     return asyncio.run(get_dag_status_from_id(env, dag, run_id))
 
 
 @airflow_router.get("/dags/query")
 def get_dag_run_status(env: Literal[*env_list] = Query(...),
-                       dag: str = Query(None, description="DAG Name",
-                                        enum=keyvault["dags-ltops"]),
+                       dag: Literal[*dag_list] = Query(...),
                        date_string: str = Query("2025-03-11T11:13:28.992416+00:00"),
                        run_state: str = Query(enum=["success", "failed"], description="Run Status")):
     return asyncio.run(get_query_response(env, dag, date_string, run_state))
@@ -36,8 +35,7 @@ def get_dag_run_status(env: Literal[*env_list] = Query(...),
 
 @airflow_router.get("/dags/logs")
 def get_dag_logs(env: Literal[*env_list] = Query(...),
-                 dag: str = Query("shapefile_ingestor_wf_status_gsm", description="DAG Name",
-                                  enum=keyvault["dags-ltops"]),
+                 dag: Literal[*dag_list] = Query(...),
                  run_id: str = Query("dc7399e8-c995-4cd1-b9f3-b6ada8836d54", description="Run Id"),
                  try_number: int = 1):
     return asyncio.run(get_dag_logs_from_run_id(env, dag, run_id, try_number))
